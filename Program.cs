654bb6e7ee41d50bc994b66f1d8459d2bf74ed20
Szymon Szewc <bilet.mejl@onet.pl>
2020-07-06 17:24:12 +0200

    Moze_teraz

--- Program.cs
+++ Program.cs
@@ -8,17 +8,7 @@ namespace kurs
         {
             string Name = args[0];
             System.Console.WriteLine("Witaj " + Name);
-            //foreach(var item in args)
-            //{
-            //    if (item == "Tomek")
-            //    {
-            //        System.Console.WriteLine("Chuj ci w dupe " + item);
-            //    }
-            //    else
-            //    {
-            //        System.Console.WriteLine("Witaj " + item, " !");
-            //    }
-            //}
+
         }
     }
 }
