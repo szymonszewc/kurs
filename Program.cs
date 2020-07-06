e22935366eb411ea6a2874febe31356875900135
Szymon Szewc <bilet.mejl@onet.pl>
2020-07-06 17:16:51 +0200

    Pierwszy

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace kurs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name = args[0];
+            System.Console.WriteLine("Witaj " + Name);
+            //foreach(var item in args)
+            //{
+            //    if (item == "Tomek")
+            //    {
+            //        System.Console.WriteLine("Chuj ci w dupe " + item);
+            //    }
+            //    else
+            //    {
+            //        System.Console.WriteLine("Witaj " + item, " !");
+            //    }
+            //}
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("kurs")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("kurs")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("39a1c5e2-9fb9-4fa8-89b7-832b747c0821")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
